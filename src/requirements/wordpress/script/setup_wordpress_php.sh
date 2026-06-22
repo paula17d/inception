c96979a8c98c0b68705904cfa70c5011540108d8
Paula Drettas <pauladrettas@gmail.com>
2026-06-22 20:07:26 +0200

    FEAT(wordpress): make sure admin username doesn't contain 'admin..' &&  FIX(wordpress): change WP_ADMIN_PASS to PASSWORD

--- src/requirements/wordpress/script/setup_wordpress_php.sh
+++ src/requirements/wordpress/script/setup_wordpress_php.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# make sure the words admin & administrator are not allowed as username
+if [[ $WP_ADMIN_USER =~ [Aa]dmin|[Aa]dministrator ]];
+then
+    echo "Error: Admin username cannot contain 'A/admin' or 'A/administrator."
+    exit 1
+fi
+
 if [ ! -f /var/www/html/index.php ]
 then
     cd /var/www/html
@@ -33,6 +40,6 @@ echo "Connected to the database."
 # configure env variables (the left side ones are in the core files)
 ./wp-cli.phar config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=${DB_HOST} --allow-root
 # send core files
-./wp-cli.phar core install --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASS} --admin_email=${WP_ADMIN_EMAIL} --allow-root
+./wp-cli.phar core install --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --allow-root
 
 exec php-fpm8.2 -F
